53683420c13f9ae38fc6cff2fe7c1365f2174522
youssefGamalMohamed <YoussefGamalMohamedElsayed@gmail.com>
2025-07-03 14:12:17 +0300

    chore: Initialized Spring API Gateway Project

---
+++ src/main/java/com/youssef/gamal/microservices/spring_cloud_api_gateway/SpringCloudApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.microservices.spring_cloud_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApiGatewayApplication.class, args);
+	}
+
+}
